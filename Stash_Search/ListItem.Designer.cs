1085233190d1f4cc1f4c3885f3372aded299570e
沈致宏 <meme755144@hotmail.com>
2020-08-08 03:56:02 +0800

    ...

---
+++ Stash_Search/ListItem.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Stash_Search
+{
+    partial class ListItem
+    {
+        /// <summary> 
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 元件設計工具產生的程式碼
+
+        /// <summary> 
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label_ItemName = new System.Windows.Forms.Label();
+            this.Label_ItemNameContent = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Label_PriceContent = new System.Windows.Forms.Label();
+            this.Label_Price = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Label_StashName = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // Label_ItemName
+            // 
+            this.Label_ItemName.AutoSize = true;
+            this.Label_ItemName.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_ItemName.Location = new System.Drawing.Point(20, 40);
+            this.Label_ItemName.Name = "Label_ItemName";
+            this.Label_ItemName.Size = new System.Drawing.Size(136, 28);
+            this.Label_ItemName.TabIndex = 0;
+            this.Label_ItemName.Text = "Item Name :";
+            // 
+            // Label_ItemNameContent
+            // 
+            this.Label_ItemNameContent.AutoSize = true;
+            this.Label_ItemNameContent.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_ItemNameContent.Location = new System.Drawing.Point(177, 40);
+            this.Label_ItemNameContent.Name = "Label_ItemNameContent";
+            this.Label_ItemNameContent.Size = new System.Drawing.Size(48, 28);
+            this.Label_ItemNameContent.TabIndex = 1;
+            this.Label_ItemNameContent.Text = "123";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panel1.Location = new System.Drawing.Point(25, 71);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 1);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panel2.Location = new System.Drawing.Point(25, 116);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 1);
+            this.panel2.TabIndex = 5;
+            // 
+            // Label_PriceContent
+            // 
+            this.Label_PriceContent.AutoSize = true;
+            this.Label_PriceContent.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_PriceContent.Location = new System.Drawing.Point(177, 85);
+            this.Label_PriceContent.Name = "Label_PriceContent";
+            this.Label_PriceContent.Size = new System.Drawing.Size(48, 28);
+            this.Label_PriceContent.TabIndex = 4;
+            this.Label_PriceContent.Text = "123";
+            // 
+            // Label_Price
+            // 
+            this.Label_Price.AutoSize = true;
+            this.Label_Price.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Price.Location = new System.Drawing.Point(20, 85);
+            this.Label_Price.Name = "Label_Price";
+            this.Label_Price.Size = new System.Drawing.Size(79, 28);
+            this.Label_Price.TabIndex = 3;
+            this.Label_Price.Text = "Price : ";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panel3.Location = new System.Drawing.Point(25, 161);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(400, 1);
+            this.panel3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 28);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "123";
+            // 
+            // Label_StashName
+            // 
+            this.Label_StashName.AutoSize = true;
+            this.Label_StashName.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_StashName.Location = new System.Drawing.Point(20, 130);
+            this.Label_StashName.Name = "Label_StashName";
+            this.Label_StashName.Size = new System.Drawing.Size(151, 28);
+            this.Label_StashName.TabIndex = 6;
+            this.Label_StashName.Text = "Stash Name : ";
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(859, 26);
+            this.panel4.TabIndex = 9;
+            // 
+            // ListItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.Label_StashName);
+            this.Controls.Add(this.Label_PriceContent);
+            this.Controls.Add(this.Label_Price);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Label_ItemNameContent);
+            this.Controls.Add(this.Label_ItemName);
+            this.Name = "ListItem";
+            this.Size = new System.Drawing.Size(859, 302);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label_ItemName;
+        private System.Windows.Forms.Label Label_ItemNameContent;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label Label_PriceContent;
+        private System.Windows.Forms.Label Label_Price;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Label_StashName;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
